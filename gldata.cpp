0e4fe2b4269019fd1cf6cdb85ed6f677f9bf8f1c
Michael Brade <brade@kde.org>
2020-04-18 20:19:34 +0200

    GLData: improve API
    - added addLine(), addTriangle(), addCuboid()
    - addVertex() is now private and used by the above
    - separate data for lines and triangles

--- gldata.cpp
+++ gldata.cpp
@@ -1,38 +1,29 @@
 #include "gldata.h"
 
 
-void GLData::addVertex(const QVector3D &a, const QVector3D &color) {
-  m_data.push_back(a.x());
-  m_data.push_back(a.y());
-  m_data.push_back(a.z());
+void GLData::addVertex(const QVector3D &a, const QVector3D &color, QVector<GLfloat> &data) {
+  data.push_back(a.x());
+  data.push_back(a.y());
+  data.push_back(a.z());
 
-  m_data.push_back(color.x());
-  m_data.push_back(color.y());
-  m_data.push_back(color.z());
+  data.push_back(color.x());
+  data.push_back(color.y());
+  data.push_back(color.z());
 }
 
 
-/**
- * Add a triangle. Front is usually where the vertices appear in counterclockwise order on
- * the screen.
- */
+void GLData::addLine(const QVector3D &a, const QVector3D &b, const QVector3D &color) {
+  addVertex(a, color, m_lines);
+  addVertex(b, color, m_lines);
+}
+
 void GLData::addTriangle(const QVector3D &a, const QVector3D &b, const QVector3D &c, const QVector3D &color) {
-  addVertex(a, color);
-  addVertex(b, color);
-  addVertex(c, color);
+  addVertex(a, color, m_tris);
+  addVertex(b, color, m_tris);
+  addVertex(c, color, m_tris);
 }
 
-
-/**
- * @short cuboid from top rectangle and thickness
- *
- * @param thickness
- * @param fracGreen
- * @param fracBlue the fraction of blue that is added to top and bottom green and red
- *
- * @param sides which sides of the cuboid to draw
- */
-void GLData::cuboid(const QVector3D &u1left, const QVector3D &u1right, const QVector3D &u2left, const QVector3D &u2right,
+void GLData::addCuboid(const QVector3D &u1left, const QVector3D &u1right, const QVector3D &u2left, const QVector3D &u2right,
                     float thickness, float fracGreen, float fracBlue, Sides sides) {
   QVector3D pnormal = QVector3D::normal(u1left, u2left, u1right) * thickness;
 
@@ -44,8 +35,8 @@ void GLData::cuboid(const QVector3D &u1left, const QVector3D &u1right, const QVe
 
   // top green
   if (sides & TOP) {
-    addTriangle(u1left, u1right, u2left, QVector3D(0, 1-fracGreen, fracBlue));
-    addTriangle(u1right, u2right, u2left, QVector3D(0, 1-fracGreen, fracBlue));
+    addTriangle(u1left, u1right, u2left, QVector3D(0, 1 - fracGreen, fracBlue));
+    addTriangle(u1right, u2right, u2left, QVector3D(0, 1 - fracGreen, fracBlue));
   }
 
   // right, front blue
@@ -72,9 +63,7 @@ void GLData::cuboid(const QVector3D &u1left, const QVector3D &u1right, const QVe
 
   // bottom red
   if (sides & BOTTOM) {
-    addTriangle(l1left, l2left, l1right, QVector3D(1-fracGreen, 0, fracBlue));
-    addTriangle(l1right, l2left, l2right, QVector3D(1-fracGreen, 0, fracBlue));
+    addTriangle(l1left, l2left, l1right, QVector3D(1 - fracGreen, 0, fracBlue));
+    addTriangle(l1right, l2left, l2right, QVector3D(1 - fracGreen, 0, fracBlue));
   }
 }
-
-
--- gldata.h
+++ gldata.h
@@ -5,16 +5,39 @@
 #include <QVector>
 #include <QVector3D>
 
+
+/**
+ * This class stores points for drawing lines and triangles.
+ */
 class GLData
 {
 public:
-  const GLfloat *constData() const { return m_data.constData(); }
+  const GLfloat *lineConstData() const      { return m_lines.constData(); }
+  const GLfloat *triangleConstData() const  { return m_tris.constData(); }
 
-  inline int count() const { return m_data.size(); }
-  inline int vertexCount() const { return count() / 6; }
+  inline int lineDataSize() const           { return m_lines.size(); }
+  inline int triangleDataSize() const       { return m_tris.size(); }
 
-  void addVertex(const QVector3D& a, const QVector3D& color);
-  void addTriangle(const QVector3D &a, const QVector3D& b, const QVector3D& c, const QVector3D& color);
+  // one vertex has 6 entries: position (x,y,z) and color (r,g,b)
+  inline int lineVertexCount() const        { return lineDataSize() / 6; }
+  inline int triangleVertexCount() const    { return triangleDataSize() / 6; }
+
+  inline void resizeLineVertexCount(int size) {
+    m_lines.resize(size * 6);
+  }
+
+
+  /**
+   * Add a line.
+   */
+  void addLine(const QVector3D &a, const QVector3D &b, const QVector3D &color);
+
+  /**
+   * Add a triangle.
+   *
+   * Front is usually where the vertices appear in counterclockwise order on the screen.
+   */
+  void addTriangle(const QVector3D &a, const QVector3D &b, const QVector3D &c, const QVector3D &color);
 
   enum Sides : char {
     NONE    = 0,
@@ -27,11 +50,24 @@ public:
     BOTTOM  = 1 << 5
   };
 
-  void cuboid(const QVector3D& u1left, const QVector3D& u1right, const QVector3D& u2left, const QVector3D& u2right,
+  /**
+   * Add a cuboid from top rectangle and thickness.
+   *
+   * @param thickness
+   * @param fracGreen
+   * @param fracBlue the fraction of blue that is added to top and bottom green and red
+   *
+   * @param sides which sides of the cuboid to draw
+   */
+  void addCuboid(const QVector3D &u1left, const QVector3D &u1right, const QVector3D &u2left, const QVector3D &u2right,
               float thickness, float fracGreen, float fracBlue, Sides sides = ALL);
 
 private:
-  QVector<GLfloat> m_data;
+  // add a vertex a with color to the given data vector
+  void addVertex(const QVector3D &a, const QVector3D &color, QVector<GLfloat> &data);
+
+  QVector<GLfloat> m_lines;
+  QVector<GLfloat> m_tris;
 };
 
-#endif // GLDATA_H
+#endif  // GLDATA_H

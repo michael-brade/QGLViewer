f058c36e46506944811c473c95212c28ae6ae469
Michael Brade <brade@kde.org>
2020-04-18 15:00:12 +0200

    CMakeLists: add QGLViewerExample executable

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
 
     QCoreApplication::setApplicationName("Qt GL Viewer Example");
-    QCoreApplication::setApplicationVersion(1.0.0);
+    QCoreApplication::setApplicationVersion("1.0.0");
 
     QGLViewer viewer;
 
